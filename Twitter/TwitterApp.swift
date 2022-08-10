dc4de65d383814cfb948d7f2ef1f93010356fe2d
cann9676 <80699111+cann9676@users.noreply.github.com>
2022-08-09 22:02:33 -0400

    Initial Commit

---
+++ Twitter/TwitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterApp.swift
+//  Twitter
+//
+//  Created by Chelsea Hannah on 8/9/22.
+//
+
+import SwiftUI
+
+@main
+struct TwitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
