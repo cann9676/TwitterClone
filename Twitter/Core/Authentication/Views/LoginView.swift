20d74dd1850016da5c181e4d1254a150b86d7097
cann9676 <80699111+cann9676@users.noreply.github.com>
2022-09-10 18:32:56 -0400

    Building Sign Up Screen

--- Twitter/Core/Authentication/Views/LoginView.swift
+++ Twitter/Core/Authentication/Views/LoginView.swift
@@ -14,22 +14,7 @@ struct LoginView: View {
         //parent container
         VStack {
             //header view
-            VStack(alignment: .leading) {
-             HStack { Spacer() }
-                
-                Text("Hello")
-                    .font(.largeTitle)
-                    .fontWeight(.semibold)
-                
-                Text("Welcome Back")
-                    .font(.largeTitle)
-                    .fontWeight(.semibold)
-            }
-            .frame(height: 260)
-            .padding(.leading)
-            .background(Color(.systemBlue))
-            .foregroundColor(.white)
-            .clipShape(RoundShape(corners: [.bottomRight]))
+           AuthHeaderView(title1: "Hello", title2: "Welcome back.")
             
             VStack(spacing: 40) {
               CustomInputField(imageName: "envelope",
