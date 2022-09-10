4e3145f84934add740aea02ca24bbf9374fbf26d
cann9676 <80699111+cann9676@users.noreply.github.com>
2022-09-10 18:06:39 -0400

    Finished Login screen UI

--- Twitter/Core/Authentication/Views/LoginView.swift
+++ Twitter/Core/Authentication/Views/LoginView.swift
@@ -52,10 +52,40 @@ struct LoginView: View {
                         .padding(.trailing, 24)
                 }
             }
+            Button {
+                print("Sign in here...")
+            } label: {
+                Text("Sign In")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .frame(width: 340, height: 50)
+                    .background(Color(.systemBlue))
+                    .clipShape(Capsule())
+                    .padding()
+            }
+            .shadow(color: .gray.opacity(0.5), radius: 10, x: 0, y: 0)
+            //adds some diminsion to UX design
             
             Spacer()
+            
+            NavigationLink {
+                RegistrationView()
+                    .navigationBarHidden(true)
+            } label: {
+                HStack  {
+                    Text("Dont have an account?")
+                        .font(.footnote)
+                    
+                    Text("Sign Up")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                }
+            }
+            .padding(.bottom, 32)
+            .foregroundColor(Color(.systemBlue))
         }
         .ignoresSafeArea()
+        .navigationBarHidden(true)
     }
 }
 
