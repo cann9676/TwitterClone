64f97ef59f274be4c2e09a2f5fa06e1b577533a2
cann9676 <80699111+cann9676@users.noreply.github.com>
2022-09-10 17:55:53 -0400

    Finishing up login screen.

--- Twitter/Core/Authentication/Views/LoginView.swift
+++ Twitter/Core/Authentication/Views/LoginView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State private var email = ""
+    @State private var password = ""
     var body: some View {
         //parent container
         VStack {
@@ -28,6 +30,28 @@ struct LoginView: View {
             .background(Color(.systemBlue))
             .foregroundColor(.white)
             .clipShape(RoundShape(corners: [.bottomRight]))
+            
+            VStack(spacing: 40) {
+                TextField("Email", text: $email)
+                
+                TextField("Password", text: $password)
+                
+            }
+            .padding(.horizontal, 32)
+            .padding(.top, 44)
+            
+            HStack {
+                 Spacer()
+                NavigationLink {
+                    Text ("Reset password view...")
+                } label: {
+                    Text("Forget Password")
+                        .font(.caption)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color(.systemBlue))
+                        .padding(.trailing, 24)
+                }
+            }
             
             Spacer()
         }
